b1c8ae9c090ac8587a4d192bdc478243cc88086d
edy-kurniawan <33270746+edy-kurniawan@users.noreply.github.com>
2025-09-01 01:46:53 +0000

    Format Blade views and fix coding style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="id">
-<head>
-  <meta charset="UTF-8">
-  <title>Connectis</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-  <div class="d-flex">
-    {{-- Sidebar --}}
-    @include('dashboard.app')
 
-    {{-- Konten Utama --}}
-    <div class="flex-grow-1 p-4">
-      @yield('content')
+<head>
+    <meta charset="UTF-8">
+    <title>Connectis</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="d-flex">
+        {{-- Sidebar --}}
+        @include('dashboard.app')
+
+        {{-- Konten Utama --}}
+        <div class="flex-grow-1 p-4">
+            @yield('content')
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
