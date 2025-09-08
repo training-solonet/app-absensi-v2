c26a1e1a645a707ca8ac8c91d59f252d4e0c2254
Firmanfebrianto <firmanarwan048@gmail.com>
2025-09-08 11:40:51 +0700

    absen baru lagi

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -7,17 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Absensi extends Model
 {
-protected $Connection='absensi_v2';
-    protected $table = 'absen'; // nama tabel di DB
-    protected $primaryKey = 'id';
-    public $timestamps = false; // kalau tabel tidak ada kolom created_at & updated_at
-
-    protected $fillable = [
-        'id_siswa',
-        'tanggal',
-        'waktu_masuk',
-        'waktu_keluar',
-        'keterangan',
-        'catatan',
-    ];
+  public function index()
+    {
+        $absen = Absen::all();
+        return view('absensi.index', compact('absen'));
+    }
 }
