42c67d69c1580e82944a4b72ca7cb87c15a59760
Firmanfebrianto <firmanarwan048@gmail.com>
2025-10-08 15:11:08 +0700

    perbaikan lagi 9

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -14,7 +14,7 @@ class Absensi extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<string>
+     * @var list<string>
      */
     protected $fillable = [
         'id_siswa',
